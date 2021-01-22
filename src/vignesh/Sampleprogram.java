cf500941d4216dc68ca12faaf70dcb6ecc3c9eef
vignesh@1603 <vigneshsporter96@gmail.com>
2021-01-22 12:07:49 +0530

    old project

---
+++ src/vignesh/Sampleprogram.java
@@ -0,0 +1,12 @@
+package vignesh;
+
+public class Sampleprogram {
+	public void shoes() {
+		System.out.println("flipkart");
+	}
+	public static void main(String[] args) {
+		Sampleprogram s = new Sampleprogram();
+		s.shoes();
+	}
+
+}
---
+++ src/vignesh/Transport.java
@@ -0,0 +1,16 @@
+package vignesh;
+
+public class Transport extends Sampleprogram {
+	public void bus() {
+		System.out.println("4 wheeler");
+	}public Transport() {
+		// TODO Auto-generated constructor stub
+	}
+	public static void main(String[] args) {
+		Transport t = new Transport();
+		t.bus();
+		t.shoes();
+		
+	}
+
+}
